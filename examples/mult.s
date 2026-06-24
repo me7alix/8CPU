a70fe3404e56fc301f938b6d3cb84293d6fc7afd
me7alix <me7alix@gmail.com>
2026-06-24 21:30:22 +0300

    change indentation

--- examples/mult.s
+++ examples/mult.s
@@ -8,18 +8,18 @@ imma 0
 sta 2
 
 loop:
-  lda 1
-  movb
-  lda 2
-  add
-  sta 2
+	lda 1
+	movb
+	lda 2
+	add
+	sta 2
 
-  lda 0
-  immb 1
-  sub
-  sta 0
+	lda 0
+	immb 1
+	sub
+	sta 0
 
-  jnz loop
+	jnz loop
 
 lda 2
 prnta
